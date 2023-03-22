bed4afbd79a5013d3a2f11d6d673f2ae76cb034f
mayles2021 <miguelangelyllaneslopez@gmail.com>
2023-03-22 13:47:42 +0100

    DudaIvan

---
+++ 007_VSCode/s004_EjerciciosPracticos/Ejercicio2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EjerciciosPracticos
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+        float val1 = 1.70F;
+        if (val1 >= 1.70){
+            Console.WriteLine("Eres apto para la oposicion");
+        } else {
+            Console.WriteLine("No Eres apto para la oposicion");
+        }
+    }
+    }
+}
---
+++ 007_VSCode/s004_EjerciciosPracticos/Ejercicio3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace EjerciciosPracticos
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+
+        int lado = 5;
+        Console.WriteLine("Superficie: " + (lado*lado));
+        saludar();  // llamar a la funcion
+        adios();
+
+        dudaIvan(lado);
+
+    }
+
+    public static void saludar(){
+        Console.WriteLine("hola soy un robot");
+    }
+
+    public static void adios(){
+        Console.WriteLine("Adios, hasta mañana ...");
+    }
+
+    public static void dudaIvan(int Tempval1){
+        Console.WriteLine("resultado" + (Tempval1*Tempval1));
+    }
+
+
+
+    }
+}
