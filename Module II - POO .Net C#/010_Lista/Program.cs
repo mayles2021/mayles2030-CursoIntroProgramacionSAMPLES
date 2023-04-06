21c127c294c89a1b56ab8d5b2a33722d4c3afeed
mayles2021 <miguelangelyllaneslopez@gmail.com>
2023-04-06 13:50:52 +0200

    Antes de puente

---
+++ Module II - POO .Net C#/010_Lista/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+namespace HelloWorld 
+{ 
+    class Hello 
+    { 
+        static void Main(string[] args) 
+        { 
+            
+             // Stack obj1 = new Stack();       // Ejemplo   
+             ArrayList obj1 = new ArrayList();
+
+             // Meter datos
+             obj1.Add("Pan");
+             obj1.Add("Lechuga");
+             obj1.Add("Leche");
+             obj1.Add("Huevos");
+
+            // Imprimir
+            foreach(string x in obj1){
+                Console.WriteLine("Cosas a comprar en el super: " + x);
+            }
+
+            // Quitar elementos de la Lista
+            obj1.Remove("Leche");
+            obj1.RemoveAt(0);           // Pan
+
+            // Imprimir
+            
+            Console.WriteLine("==================================");
+            foreach(string x in obj1){
+                Console.WriteLine("Cosas a comprar en el super: " + x);
+            }
+
+
+        } 
+    }
+}
---
+++ Module II - POO .Net C#/011_ListasString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+namespace HelloWorld 
+{ 
+    class Hello 
+    { 
+        static void Main(string[] args) 
+        { 
+
+            // Listas + TipoDatos
+            List<string> obj1 = new List<String>();
+            string[] objString = new string[5];
+
+            // Añadir datos al array modelos coches
+            objString[0] = "BMW";
+            objString[1] = "Renault";
+            objString[2] = "Citroen";
+            objString[3] = "Mercedes";
+            objString[4] = "Ferrari";
+
+            // Imprimir
+            for(int i=0; i<=4; i++){
+                Console.WriteLine("Valor: " + objString[i]);
+            }
+
+            // Rellenar Lista con los valores array
+            obj1.AddRange(objString);
+
+            // Imprimir Lista
+            foreach(string x in obj1) {
+                Console.WriteLine(x);
+            }
+            Console.ReadKey();
+
+        } 
+    }
+}
