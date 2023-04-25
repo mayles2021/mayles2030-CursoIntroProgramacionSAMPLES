278322641eff2711db1587753c36d8b96c93e4fc
mayles2021 <miguelangelyllaneslopez@gmail.com>
2023-04-25 11:38:32 +0200

    LinkedList

---
+++ Module II - POO .Net C#/036_LinkedList/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace HelloWorld 
+{ 
+    class Hello 
+    { 
+        static void Main(string[] args) 
+        { 
+            
+            LinkedList<string> my_list = new LinkedList<string>();
+            my_list.AddLast("Miguel");
+            my_list.AddLast("Rafael");
+            my_list.AddLast("Antonio");
+            my_list.AddLast("Pepe");
+           
+            foreach(string str1 in my_list){
+                Console.WriteLine(str1);
+            }
+
+            my_list.RemoveLast();
+            my_list.RemoveFirst();
+            my_list.Remove("Pepe");
+            
+            foreach(string str1 in my_list){
+                Console.WriteLine(str1);
+            }
+
+        } 
+    }
+}
